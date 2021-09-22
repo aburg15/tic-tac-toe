780e20022467ecce6e568326d3e39a0e6ccb0723
Adam Burgess <aburg1515@gmail.com>
2021-09-22 16:19:29 -0600

    Add basic HTML and CSS skeleton along with font import

---
+++ game.js
@@ -0,0 +1,10 @@
+class Game {
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
+    this.playerTurn = player1;
+    this.gameResult;
+    this.resetGame;
+    this.winningCombo;
+  }
+}
