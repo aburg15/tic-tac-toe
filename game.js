49f971f49b1d9674b550730219abf8c5c9b7f135
Adam Burgess <aburg1515@gmail.com>
2021-09-26 10:51:25 -0600

    Add showPlayerLogo and updateGameBoard functions

--- game.js
+++ game.js
@@ -1,10 +1,11 @@
 class Game {
-  constructor() {
-    this.player1 = new Player('player1');
-    this.player2 = new Player('player2');
+  constructor(player1, player2) {
+    this.player1 = player1;
+    this.player2 = player2;
     this.playerTurn = this.player1;
-    this.gameResult;
-    this.resetGame;
+    this.board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    this.win = false;
+    this.turnNumber = 0;
     this.winningCombo =
     [[0, 1, 2],
     [3, 4, 5],
@@ -15,4 +16,21 @@ class Game {
     [2, 4, 6],
     [2, 5, 8]];
   }
+
+  boxSelected(chosenBox) {
+    if (this.player1.logo && this.player2.logo !== this.board[chosenBox]) {
+      this.addPlayerLogo(chosenBox, this.playerTurn);
+    }
+  }
+
+  addPlayerLogo(chosenBox, playerTurn) {
+    for (var i = 0; i < this.board.length; i++) {
+      console.log('a', chosenBox)
+      if (this.board[i] === chosenBox) {
+        this.board.splice(i, 1, this.playerTurn.logo);
+        this.turnNumber += 1;
+      }
+    }
+  }
+
 }
--- main.js
+++ main.js
@@ -3,9 +3,10 @@ var game;
 
 // Query Selectors
 var boxes = document.querySelector('.game-board-container');
-var gameBoardBoxes = document.querySelector('#boxes')
-var currentTurnDisplay = document.querySelector('#currentTurnDisplay')
-
+var gameBoardBoxes = document.querySelector('#boxes');
+var currentTurnDisplay = document.querySelector('#currentTurnDisplay');
+var playerOneWinTotal = document.querySelector('#playerOneWinsDisplay');
+var playerTwoWinTotal = document.querySelector('#playerTwoWinsDisplay');
 
 // Event Listeners
 window.addEventListener('load', onPageLoad)
@@ -15,36 +16,37 @@ boxes.addEventListener('click', playerSelectsBox);
 
 // Functions
 function onPageLoad() {
+  var player1 = new Player('player1', 'X');
+  var player2 = new Player('player2', 'O');
+  game = new Game(player1, player2);
   showPlayerTurn();
-  // displayWinTotal();
+  showWinTotal();
 }
 
 function showPlayerTurn() {
-  game = new Game();
   if (game.playerTurn === game.player1) {
-    console.log('hi')
-    currentTurnDisplay.innerHTML += `<img src="assets/mario.png" class="mario-image">`
+    currentTurnDisplay.innerHTML += `<img src="assets/mario.png" class="mario-image"> MARIO'S TURN!`
   } else if (game.playerTurn === game.player2) {
-    currentTurnDisplay.innerHTML += `<img src="assets/DK.png" class="DK-image">`
+    currentTurnDisplay.innerHTML += `<img src="assets/DK.png" class="DK-image"> DONKEY KONG'S TURN!`
   }
 }
 
+function showWinTotal() {
+  playerOneWinTotal.innerHTML = game.player1.wins;
+  playerTwoWinTotal.innerHTML = game.player2.wins;
+}
 
 
 function playerSelectsBox(event) {
-  // place x or o
-  var selectBox = event.target;
-  console.log(selectBox)
-  displayGamePiece();
-  // check for win
-  // check for draw
-  // if none of the above are met we need to switch turns
+  if(game.win === false) {
+    showPlayerLogoInBox(event.target);
+    game.boxSelected(event.target.id);
+  }
+
 }
 
-function displayGamePiece() {
-  if (!gameBoardBoxes.innerHTML) {
-    gameBoardBoxes.innerHTML =
-      `<h1>X</h1>`
-    changeTurns = false;
+function showPlayerLogoInBox(boxSelected) {
+  if(boxSelected.innerText === '') {
+    boxSelected.innerText = game.playerTurn.logo
   }
 }
